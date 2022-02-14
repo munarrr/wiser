93add6ed0a6053013ec0cc685c9aca737a682f76
munarrrrr <karymshakovmunar@gmail.com>
2022-02-14 21:46:42 +0600

    updated

---
+++ src/components/logo/index.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "gatsby";
+import PropTypes from "prop-types";
+import cn from "classnames";
+import Image from "@ui/image";
+import { ImageType } from "@utils/types";
+import Avatar from '../../data/images/logo/logo-06.png'
+
+const Logo = ({ className }) => {
+    return (
+        <div className={cn("logo", className)}>
+            <Link to="/">
+                    <Image src={Avatar} alt="logo" />
+            </Link>
+        </div>
+    );
+};
+
+Logo.propTypes = {
+    className: PropTypes.string,
+    image: PropTypes.shape(ImageType),
+};
+
+export default Logo;
---
+++ src/layouts/index.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import PropTypes from "prop-types";
+import cn from "classnames";
+import ScrollToTop from "@ui/scroll-to-top";
+import SEO from "@components/seo";
+import "@assets/css/bootstrap.min.css";
+import "@assets/scss/style.scss";
+import "aos/dist/aos.css";
+import Client from "./client";
+
+const Layout = ({ children, color, className, pageTitle }) => {
+    return (
+        <>
+            <SEO
+                bodyClass={cn(`template-color-${color} spybody`, className)}
+                titleTemplate={pageTitle}
+            />
+            <Client />
+            {children}
+            <ScrollToTop />
+        </>
+    );
+};
+
+Layout.propTypes = {
+    children: PropTypes.node.isRequired,
+    color: PropTypes.oneOf([1, 2]),
+    className: PropTypes.string,
+    pageTitle: PropTypes.string,
+};
+
+Layout.defaultProps = {
+    color: 1,
+};
+
+export default Layout;
